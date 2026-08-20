ddee73471d3d5ff4b92873112722b9da15b26731
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-19 23:05:59 -0700

    feat(services): establish application service package

---
+++ neural_search/services/__init__.py
@@ -0,0 +1,5 @@
+"""Application services that orchestrate Neural Search domain capabilities."""
+
+from neural_search.services.runtime_readiness import RuntimeReadinessService
+
+__all__ = ["RuntimeReadinessService"]
