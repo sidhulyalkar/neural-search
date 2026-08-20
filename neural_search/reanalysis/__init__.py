5cd4043c25fce8482228a9ec152ce55d9f2b6e46
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-19 23:59:05 -0700

    feat(reanalysis): add first-class reanalysis planning package

---
+++ neural_search/reanalysis/__init__.py
@@ -0,0 +1,15 @@
+"""Evidence-aware planning for dataset reuse and reanalysis."""
+
+from neural_search.reanalysis.planner import (
+    ReanalysisCandidate,
+    ReanalysisEvidence,
+    ReanalysisPlan,
+    build_reanalysis_plan,
+)
+
+__all__ = [
+    "ReanalysisCandidate",
+    "ReanalysisEvidence",
+    "ReanalysisPlan",
+    "build_reanalysis_plan",
+]
